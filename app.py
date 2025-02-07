716a16ca51c76c1123349c5e6a6b17b24d575ca1
Coco10130 <teneperejohnric65@gmail.com>
2025-02-07 19:31:07 +0800

    test

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
 import requests
 
 app = Flask (__name__)
+CORS(app)
 
 @app.route("/")
-def index():
+def index() :
+    return "This is flask"
+
+@app.route("/api/cat-jokes", methods=["GET"])
+def cat_jokes():
     try:
         response = requests.get('https://catfact.ninja/fact')
         data = response.json()
-        return jsonify(data)  # Returning the cat fact as JSON
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@app.route("/api/dog-pic", methods=["GET"])
+def dog_pic():
+    try:
+        response = requests.get('https://dog.ceo/api/breeds/image/random')
+        data = response.json()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/api/jokes", methods=["GET"])
+def random_jokes():
+    try:
+        response = requests.get('https://v2.jokeapi.dev/joke/Programming?type=twopart')
+        data = response.json()
+        return jsonify(data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
